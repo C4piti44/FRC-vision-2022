4583249b732d4d590c3295b01b199642eb7cab1f
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-03-04 17:11:55 +0200

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,1 +1,18 @@
+import numpy as np
+import cv2
 
+video = cv2.VideoCapture(0)
+
+lower_red = np.array([0,150 ,20])
+upper_red = np.array([15 ,255,255])
+
+
+while True :
+    success , img = video.read()
+    image = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(image , lower_red , upper_red)
+
+    cv2.imshow("mask" , mask)
+    cv2.imshow("webcam", img)
+
+    cv2.waitKey(2)
