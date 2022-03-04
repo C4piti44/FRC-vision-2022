02d88b6b55e81c13d2f94b74931038fb9c6a3d84
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-03-04 18:12:08 +0200

    Update vision.py

--- vision.py
+++ vision.py
@@ -3,14 +3,22 @@ import cv2
 
 video = cv2.VideoCapture(0)
 
-lower_red = np.array([0,150 ,20])
-upper_red = np.array([15 ,255,255])
+lower_red = np.array([100, 120 ,20])
+upper_red = np.array([132 ,250,255])
 
 
 while True :
     success , img = video.read()
     image = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(image , lower_red , upper_red)
+    
+    contoues , hierarchy = cv2.findContours(mask , cv2.RETR_EXTRERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    if len(contours) != 0:
+        for contour in contours :
+            if cv2.contourArea(contour) > 500: 
+                x,y,w,h = cv2.boundingRect(contour)
+                cv2.rectangle(img , (x,y) , (x+w,y+h) (0,0,255) , 3)
 
     cv2.imshow("mask" , mask)
     cv2.imshow("webcam", img)
